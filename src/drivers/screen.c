130c90723b64fcec8defa13a047d14d915ec47ba
Brian Chen <brianchen.chen@mail.utoronto.ca>
2021-01-06 19:25:03 -0800

    scrolling

--- src/drivers/screen.c
+++ src/drivers/screen.c
@@ -1,8 +1,8 @@
 #include "screen.h"
 #include "ports.h"
+#include "../kernel/util.h"
 
 // private functions 
-
 int get_cursor_offset();
 void set_cursor_offset(int offset);
 int print_char(char c, int col, int row, char attr);
@@ -10,7 +10,49 @@ int get_offset(int col, int row);
 int get_offset_row(int offset);
 int get_offset_col(int offset);
 
+// public kernel functions
 
+void kprint_at(char* message, int col, int row) {
+    int offset;
+    // set cursor to current cursor offset if col/row are negative
+    if (col >= 0 && row >= 0) {
+        offset = get_offset(col, row);
+    } else {
+        offset = get_cursor_offset();
+        row = get_offset_row(offset);
+        col = get_offset_row(offset);
+    }
+
+    // print msg w/ loop
+    //
+    int i = 0;
+    while (message[i] != 0) {
+        offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    } 
+
+}
+
+void kprint(char* message) {
+    kprint_at(message, -1, -1);
+}
+
+
+
+void clear_screen() {
+    int screen_size = MAX_COLS * MAX_ROWS;
+    char* screen = (char*) VIDEO_ADDRESS;
+    int i;
+
+    for (i = 0; i < screen_size; i++) { 
+        screen[i*2] = ' ';
+        screen[i*2+1] = WHITE_ON_BLACK;
+    }
+    set_cursor_offset(get_offset(0,0));
+}
+
+// private func definition
 int print_char(char c, int col, int row, char attr) {
     unsigned char* vidmem = (unsigned char *) VIDEO_ADDRESS;
     if (!attr) attr = WHITE_ON_BLACK; // default display style
@@ -35,6 +77,23 @@ int print_char(char c, int col, int row, char attr) {
         vidmem[offset] = c;
         vidmem[offset+1] = attr;
         offset += 2;
+    } 
+
+    // scroll if offset is over screen size
+    //
+    if (offset >= MAX_ROWS * MAX_COLS * 2) {
+        int i;
+        for (i = 1; i < MAX_ROWS; i++) {
+            memory_copy(get_offset(0, i) + VIDEO_ADDRESS,
+                    get_offset(0, i-1) + VIDEO_ADDRESS,
+                    MAX_COLS *2);
+        }
+        // blank last line
+        char* last_line = (char*) get_offset(0, MAX_ROWS-1) + VIDEO_ADDRESS;
+        for (i = 0; i < MAX_COLS*2; i++) {
+            last_line[i] = 0;
+        }
+        offset -= 2*MAX_COLS;
     }
 
     set_cursor_offset(offset);
@@ -44,8 +103,6 @@ int print_char(char c, int col, int row, char attr) {
 
 int get_cursor_offset() {
     // use vga ports to get current cursor position
-    
-
     port_byte_out(REG_SCREEN_CTRL, 14);
     // high byte << 8
     int offset = port_byte_in(REG_SCREEN_DATA) << 8; 
@@ -70,64 +127,5 @@ void set_cursor_offset(int offset) {
 int get_offset(int col, int row) { return 2 * (row * MAX_COLS + col); }
 int get_offset_row(int offset) { return offset / (2 * MAX_COLS); }
 int get_offset_col(int offset) { return (offset - (get_offset_row(offset)*2*MAX_COLS))/2; }
-
-
-
-
-
-
-
-// public kernel functions
-
-void kprint_at(char* message, int col, int row) {
-    int offset;
-    // set cursor to current cursor offset if col/row are negative
-    if (col >= 0 && row >= 0) {
-        offset = get_offset(col, row);
-    } else {
-        offset = get_cursor_offset();
-        row = get_offset_row(offset);
-        col = get_offset_row(offset);
-    }
-
-    // print msg w/ loop
-    //
-    int i = 0;
-    while (message[i] != 0) {
-        offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
-        row = get_offset_row(offset);
-        col = get_offset_col(offset);
-    }
-
-}
-
-void kprint(char* message) {
-    kprint_at(message, -1, -1);
-}
-
-
-
-void clear_screen() {
-    int screen_size = MAX_COLS * MAX_ROWS;
-    char* screen = (char*) VIDEO_ADDRESS;
-    int i;
-
-    for (i = 0; i < screen_size; i++) { 
-        screen[i*2] = ' ';
-        screen[i*2+1] = WHITE_ON_BLACK;
-    }
-    set_cursor_offset(get_offset(0,0));
-}
-
-
-
-
-
-
-
-
-
-
-
 
 
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,8 +4,11 @@
 
 void main() {
     clear_screen();
-    char* hello_world = "hello_world";
-    kprint(hello_world);
+    kprint_at("X", 1, 6);
+    kprint_at("This text spans multiple lines", 75, 10);
+    kprint_at("There is a line\nbreak", 0, 20);
+    kprint("There is a line\nbreak");
+    kprint_at("What happens when we run out of space?", 45, 24);
 }
 
 
